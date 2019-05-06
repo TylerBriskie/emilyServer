7eb5a8c89fe01258f76ddd8e220847d4c415bc15
Tyler Briskie <tylerbriskie@gmail.com>
2019-05-05 20:38:13 -0600

    add validation to post new client

--- routes/clients.js
+++ routes/clients.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const Client = require('../models/client');
 
 router.post('/', (req, res) => {
-  // collection.insert(req.body, (error, result) => {
-  //   if (error){
-  //     console.log(error);
-  //     return res.status(500).send(error);
-  //   }
-  //   res.send(result.result);
-  // });
+    if (!req.body.email){
+        res.status(422).send("Cannot create customer without email");
+    }
+    if (!req.body.password){
+        res.status(422).send("Cannot create customer without password");
+    }
     let clientData = req.body;
     let client = new Client(clientData);
     client.save((error, client) => {
@@ -23,21 +22,20 @@ router.post('/', (req, res) => {
 
 router.get('/', (req, res) => {
     res.status(200).send('hi');
-  // collection.find({}).toArray((error, result) => {
-  //         if(error) {
-  //             return res.status(500).send(error);
-  //         }
-  //         res.send(result);
-  //     });
+
 });
 
 router.get('/:id', (req, res) => {
-  // collection.findOne({"client_id": new ObjectId(req.params.client_id)}, (error, result) => {
-  //         if(error) {
-  //             return res.status(500).send(error);
-  //         }
-  //         res.send(result);
-  //     });
+    console.log('finding client by poop pants: ', req.params.id);
+    let clientId = req.params.id;
+    Client.findById(clientId, (err, client)=> {
+        if (err){
+            console.log('error: ', err);
+            res.status(401).send('Client Not Found');
+        } else {
+            res.status(200).send(client);
+        }
+    });
 });
 
 
